0fd68f95541d038494bfd8174a2c563ea3224873
Thomas Lau <thomasklau@gmail.com>
2017-07-12 17:49:12 -0700

    Extended time for enhanced sampling guide

--- 1_dock/xdock_ligand_dir_to_grid_dir.sh
+++ 1_dock/xdock_ligand_dir_to_grid_dir.sh
@@ -7,5 +7,5 @@ OUT=$3
 
 for grid in $(ls $GRIDS)
 do
-    sbatch --time=20:00:00 --job-name=$grid -n 1 -p rondror /share/PI/rondror/$USER/combind/1_dock/xdock_ligand_dir_to_single_grid.sh $GRIDS/$grid/$grid.zip $LIGANDS $OUT &
+    sbatch --time=200:00:00 --job-name=$grid -n 1 -p rondror /share/PI/rondror/$USER/combind/1_dock/xdock_ligand_dir_to_single_grid.sh $GRIDS/$grid/$grid.zip $LIGANDS $OUT &
 done
