9777bb2ce51e837ffa4fba492290e614c8dd91dd
Robin Betz <robin@robinbetz.com>
2017-08-18 15:38:44 -0700

    Change parallel steps to use a pool of workers instead of using that pool to create a bunch of single-CPU jobs. Increase resources to xdock.py so the pool has some CPUs in it Change permissions so everything isn't executable

--- 1_dock/xdock.sh
+++ 1_dock/xdock.sh
@@ -1,7 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 #SBATCH --time=12:00:00
 #SBATCH --partition=rondror
+#SBATCH --tasks=4 --cpus-per-task=1
+#SBATCH --tasks-per-socket=2 --nodes=1
+#SBATCH --mem=8GB
+#SBATCH --job-name=xdock
+#SBATCH --output=xdock.out --open-mode=append
 
 ACTION=$1
 DATASET=$2
