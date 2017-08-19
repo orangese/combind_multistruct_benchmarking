a5469122d522b1b7b7ab5c09539221d9c69e77ab
juliabelk <you@example.com>
2017-08-19 10:05:44 -0700

    fixed directory error

--- 1_dock/prep_pdbbind.py
+++ 1_dock/prep_pdbbind.py
@@ -8,9 +8,10 @@ uniprot_index = '{}/index/INDEX_refined_name.2016'.format(PDBBIND)
 
 command = sys.argv[1]
 
-output = '/scratch/PI/rondror/docking_data/pdbbind'
+all_datasets = '/scratch/PI/rondror/docking_data/pdbbind'
+all_code = '/share/PI/rondror/$USER/combind/1_dock'
 
-os.chdir(PDBBIND)
+#os.chdir(PDBBIND)
 
 def parse_index_file():
     test = {}
@@ -25,19 +26,21 @@ def parse_index_file():
             test[uniprot_id].append(pdb_code)
     return test
 
-os.chdir(output)
-
+#os.chdir(output)
+os.chdir(all_datasets)
 if command == '-m':
+    #os.chdir(all_datasets)
     for s in os.listdir('.'):
-        os.system('rm -r {}/ligands {}/raw_pdbs'.format(s, s))
+        if 'ligands' in os.listdir(s) or 'raw_pdbs' in os.listdir(s): continue
         os.system('mkdir {}/ligands'.format(s))
         os.system('mkdir {}/raw_pdbs'.format(s))
         for pdb in os.listdir('{}/original_files'.format(s)):
             os.system('cp {}/original_files/{}/{}_ligand.mol2 {}/ligands/{}_ligand.mol2'.format(s, pdb, pdb, s, pdb.upper()))
             os.system('cp {}/original_files/{}/{}_protein.pdb {}/raw_pdbs/{}.pdb'.format(s, pdb, pdb, s, pdb.upper()))
+
 if command in ['-r', '-s']:
     num_folders = len(os.listdir('.'))
     for i, s in enumerate(os.listdir('.')):
         num_structs = len(os.listdir('{}/raw_pdbs'.format(s)))
         print 'running {} on {} structures in dataset {}. {} datasets remaining.'.format(command, num_structs, s, num_folders - i)
-        os.system('./main.py {} pdbbind/{}'.format(command, s))
+        os.system('{}/main.py {} pdbbind/{}'.format(all_code, command, s))
