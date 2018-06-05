42d75d1e69f3f3e00751aeca5c3413bf55224e7a
juliabelk <you@example.com>
2018-06-05 10:37:28 -0700

    dock

--- 1_dock/dock.py
+++ 1_dock/dock.py
@@ -1,109 +1,86 @@
 import os
 import sys
 
+import itertools
+
+group_size=10
+def grouper(n, iterable, fillvalue=None):
+    args = [iter(iterable)] * n 
+    return itertools.izip_longest(*args, fillvalue=fillvalue)
+
 XGLIDE_IN = '''GRIDFILE   ../../grids/{}/{}.zip
-LIGANDFILE   ../../../{}/{}/{}.mae
+LIGANDFILE   ../../../ligands/prepared_ligands/{}/{}.mae
 DOCKING_METHOD   confgen
 CANONICALIZE   True
-EXPANDED_SAMPLING   {}
+EXPANDED_SAMPLING   False
 POSES_PER_LIG   300
 POSTDOCK_NPOSE   300
 WRITEREPT   True
-PRECISION   {}
-NENHANCED_SAMPLING   {}
+PRECISION   SP
+NENHANCED_SAMPLING   2
 '''
 
-#ref_lig = '''USE_REF_LIGAND   True
-#REF_LIGAND_FILE   ../{}
-#CORE_DEFINITION   allheavy
-#'''
-
-peptide_settings = '''MAXKEEP 100000
-MAXREF 1000'''
-
-core_def = '''USE_REF_LIGAND   True
-REF_LIGAND_FILE   ../../core/{}
-CORE_DEFINITION   allheavy
-CORE_RESTRAIN   True
-CORE_POS_MAX_RMSD   2.0
-CORECONS_FALLBACK   True'''
-
-settings = { # peptides, pr, exp, enh, lig_source
-    'glide1': (False, 'SP', 'True',  1, 'lig_raw'),
-    'glide2': (False, 'XP', 'True',  1, 'lig_raw'),
-    'glide3': (False, 'SP', 'False', 1, 'ligands/unique'),
-    'glide4': (False, 'XP', 'False', 1, 'lig_raw'),
-    'glide5': (False, 'SP', 'False', 1, 'lig_opt'),
-    'glide6': (False, 'XP', 'False', 1, 'lig_opt'),
-    'glide7': (True,  'SP', 'False', 1, 'lig_raw'),
-    'glide8': (True,  'SP', 'True',  1, 'lig_raw'),
-    'glide9': (True,  'SP', 'True',  4, 'lig_raw'),
-    'glide10':(True,  'SP', 'False', 4, 'lig_raw'),
-    'glide11':(False, 'SP', 'False', 4, 'ligands/unique'),
-    'glide12':(False, 'SP', 'False', 2, 'ligands/prepared_ligands')
-}
-
 queue = 'rondror'
+out_dir = 'docking/glide12'
 
-def glide_exists(ligand, grid, out_dir):
-    pv_exists = os.path.exists('docking/{}/{}-to-{}/{}-to-{}_pv.maegz'.format(out_dir, ligand, grid, ligand, grid))
-    rmsd_exists = os.path.exists('docking/{}/{}-to-{}/rmsd.csv'.format(out_dir, ligand, grid))
-    if ligand[:6] == 'CHEMBL': return pv_exists
-    return pv_exists and rmsd_exists
+dock_cmd = '$SCHRODINGER/glide -WAIT {}-to-{}.in\n' 
+rmsd_cmd = '$SCHRODINGER/run rmsd.py -use_neutral_scaffold -pv second -c rmsd.csv ../../../structures/ligands/{}.mae {}-to-{}_pv.maegz\n'
 
-def glide_failed(ligand, grid, out_dir):
-    log_file = 'docking/{}/{}-to-{}/{}-to-{}.log'.format(out_dir, ligand, grid, ligand, grid)
-    txt1 = 'Total elapsed time'
-    txt2 = 'GLIDE FATAL ERROR'
-    return os.path.exists(log_file) and (txt1 in open(log_file).read() or txt2 in open(log_file).read())
+def get_state(ligand, grid):
+    pv = '{}-to-{}/{}-to-{}_pv.maegz'.format(ligand, grid, ligand, grid)
+    rmsd = '{}-to-{}/rmsd.csv'.format(ligand, grid)
+    log = '{}-to-{}/{}-to-{}.log'.format(ligand, grid, ligand, grid)
+    ref_lig = '../../structures/ligands/{}.mae'.format(ligand)
+    inp_lig = '../../ligands/prepared_ligands/{}/{}.mae'.format(ligand, ligand)
+    inp_grid = '../grids/{}/{}.zip'.format(grid, grid)
 
-def dock_pair(ligand, grid, out_dir, restrain=None):
-    pv_exists = os.path.exists('docking/{}/{}-to-{}/{}-to-{}_pv.maegz'.format(out_dir, ligand, grid, ligand, grid))
-    rmsd_exists = os.path.exists('docking/{}/{}-to-{}/rmsd.csv'.format(out_dir, ligand, grid))
-    #if pv_exists: print ligand
-    if pv_exists and (rmsd_exists or len(ligand.split('_')[0]) > 4):
-        return
-    if not pv_exists and glide_failed(ligand, grid, out_dir):
-        return
+    # 0: do nothing
+    # 1: compute rmsd
+    # 2: dock
 
-    #if glide_exists(ligand, grid, out_dir) or glide_failed(ligand, grid, out_dir):
-    #    return
-    if not os.path.exists('docking/grids/{}/{}.zip'.format(grid, grid)): return
-    print ligand, grid, out_dir
-    os.system('mkdir -p docking/{}'.format(out_dir))
-    os.chdir('docking/{}'.format(out_dir))
-    time = '00:30:00'
-    if '{}-to-{}'.format(ligand, grid) in os.listdir('.'):
-        time = '02:30:00'
-        os.system('rm -rf {}-to-{}'.format(ligand, grid))
+    if os.path.exists(rmsd): 
+        return 0
+    if os.path.exists(pv):
+        if os.path.exists(ref_lig): return 1
+        else: return 0
+    if os.path.exists(log):
+        logtxt = open(log).read()
+        if 'Total elapsed time' in logtxt or 'GLIDE FATAL ERROR' in logtxt:
+            return 0
+    if not os.path.exists(inp_lig) or not os.path.exists(inp_grid):
+        return 0
 
-    peptides, precision, exp, nenhanced, lig_source = settings[out_dir]
+    os.system('rm -rf {}-to-{}'.format(ligand, grid))
+    return 2
 
-    os.system('mkdir {}-to-{}'.format(ligand, grid))
-    ref_lig_file = '../../../structures/ligands/{}.mae'.format(ligand)
+def write_inp_files(all_pairs):
+    os.system('rm -f *.sh')
+    for ligand, grid in all_pairs:
+        os.system('mkdir {}-to-{}'.format(ligand, grid))
+        with open('{}-to-{}/{}-to-{}.in'.format(ligand, grid, ligand, grid), 'w') as f:
+            f.write(XGLIDE_IN.format(grid, grid, ligand, ligand))
 
-    with open('{}-to-{}/{}-to-{}.in'.format(ligand, grid, ligand, grid), 'w') as f:
-        f.write(XGLIDE_IN.format(grid, grid, lig_source, ligand, ligand, exp, precision, nenhanced, lig_source, ligand))
-        if peptides:
-            f.write(peptide_settings)
-        #elif ligand[:6] != 'CHEMBL': 
-        #    os.chdir('../..')
-        #    return # wait until the reference ligand is available
+def proc_all(all_pairs, dock=False, rmsd=False):
+    
+    for i,group in enumerate(grouper(group_size, all_pairs)):
+        with open('dock{}.sh'.format(i),'w') as f:
+            f.write('#!/bin/bash\nmodule load schrodinger\n')
+            for pair in group:
+                if pair is None: continue
+                ligand, grid = pair
+                f.write('cd {}-to-{}\n'.format(ligand, grid))
+                if dock:
+                    f.write(dock_cmd.format(ligand, grid))
+                if rmsd:
+                    f.write(rmsd_cmd.format(ligand, ligand, grid))
+                f.write('cd ..\n')
 
-        if restrain is not None:
-            f.write(core_def.format(restrain))
-
-    with open('{}-to-{}/{}-{}.sh'.format(ligand, grid, ligand, grid), 'w') as f:
-        f.write('#!/bin/bash\nmodule load schrodinger\n')
-        f.write('$SCHRODINGER/glide -WAIT {}-to-{}.in\n'.format(ligand, grid))
-        if ligand[:6] != 'CHEMBL':
-            f.write('$SCHRODINGER/run rmsd.py -use_neutral_scaffold -pv second -c rmsd.csv {} {}-to-{}_pv.maegz'.format(ref_lig_file, ligand, grid))
-
-    os.chdir('{}-to-{}'.format(ligand, grid))
-    os.system('sbatch -p {} -t {} -o dock.out {}-{}.sh'.format(queue, time, ligand, grid))
-    os.chdir('../../..')
+        os.system('sbatch -p {} -t 1:00:00 -o dock.out dock{}.sh'.format(queue, i))
 
 def dock(lm, chembl=None, maxnum=20):
+
+    os.system('mkdir -p {}'.format(out_dir))
+    os.chdir(out_dir)
 
     if chembl is None:
         ligs = lm.pdb[:maxnum]
@@ -116,11 +93,21 @@ def dock(lm, chembl=None, maxnum=20):
                 ligs.add(q)
                 ligs.update(c_list)
 
+    to_dock = []
+    to_rmsd = []
     for lig in ligs:
         for grid in grids:
-            dock_pair(lig, grid, 'glide12')
+            s = get_state(lig, grid)
+            if s == 2: to_dock.append((lig, grid))
+            if s == 1: to_rmsd.append((lig, grid))
             
+    if len(to_dock) > 0:
+        print 'docking {} pairs'.format(len(to_dock))
+        write_inp_files(to_dock)
+        proc_all(to_dock, dock=True)
+    if len(to_rmsd) > 0:
+        print 'computing {} rmsds'.format(len(to_rmsd))
+        proc_all(to_rmsd, rmsd=True)
 
-
-
+    os.chdir('../..')
 
