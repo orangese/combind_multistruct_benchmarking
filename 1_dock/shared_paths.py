813d99464c298091e0349d4be693a4c617992515
Helena Miranda Roberts-Mataric <helenarm@sh-ln07.stanford.edu>
2018-07-03 10:39:12 -0700

    changed shared paths

--- 1_dock/shared_paths.py
+++ 1_dock/shared_paths.py
@@ -1,6 +1,6 @@
 shared_paths = { 
-    'code'      : '/scratch/PI/rondror/jbelk/method/combind',
-    'data'      : '/scratch/PI/rondror/jbelk/method/data',
+    'code'      : '/scratch/PI/rondror/helenarm/combind',
+    'data'      : '/scratch/PI/rondror/helenarm/data',
     'docking'   : 'glide12',
     'mcss'      : 'custom_mcss',
     'mcss_type' : 'mcss14',
